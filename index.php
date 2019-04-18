14d72451e4358d1f80470353f9adc97a90fd5da6
helmisendypermadi <helmisendyp@gmail.com>
2019-04-19 05:33:13 +0700

    1.commit index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Website Belajar Coding Bahasa Indonesia</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<!-- https://github.com/helmisendypermadi/TubesWeb.git -->
