632a88a8eab58e7923e7f1d47948b5aec17ca723
helmisendypermadi <helmisendyp@gmail.com>
2019-04-22 11:23:22 +0700

    pertama

--- login.php
+++ login.php
@@ -21,14 +21,21 @@
 	<!-- Tag meta agar kompatibel dengan Internet Explorer -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<!-- End Tag meta agar kompatibel dengan Internet Explorer -->
-	<!-- <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> -->
-	<link href="index.css" type="text/css" rel="stylesheet" media="all" >
-	<!-- <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script> -->
-	<title>Website Belajar Coding Bahasa Indonesia</title>
+
+	<!-- Google Font Online -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+	<!-- End Google Font Online -->
+
+	<!-- CSS external -->
+	<link href="login.css" type="text/css" rel="stylesheet" media="all" >
+	<!-- CSS external -->
+
+	<title>Login-CodePolitan.com</title>
 	<link rel="shortcut icon" href="image/favicon.png">
 </head>
-<body>
-<!-- BOOTSTRAP -->
+<body data-smooth-scroll-ofset="77" cz-shortcut-listen="true">
+
+<!-- BOOTSTRAP HEADER-->
 <nav class="navbar navbar-expand-lg navbar-light bg-white" style="font-size: 0.857142857142857em" >
   <a class="navbar-brand" href="#">
   	<img class="gambar" src="image/codepolitan.png" alt="Logo">
@@ -41,7 +48,7 @@
     <ul class="navbar-nav mr-auto">
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <strong>LEARNING PROGRAMS</strong>
+          <strong style="margin-left: -35px;">LEARNING PROGRAMS</strong>
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#">Online Course</a>
@@ -59,59 +66,17 @@
         <a class="nav-link" href="#"><strong>FORUM</strong></a>
       </li>
     </ul>
-    <!-- <form class="form-inline my-2 my-lg-0" >
-      <button type="button" class="btn btn-light" style="margin-right: 20px;">LOGIN</button>
-    </form> -->
-    <div class="login">
-    	<a id="btn-page-login" class="btn tbn--sm tkype--uppercase" href="">
+    <div class="login" style="margin-right: 25px;">
+    	<a id="btn-page-login" class="btn btn--sm type--uppercase" href="login.php">
     		<span class="btn__text">LOGIN</span>
     	</a>
     </div>
      <form class="form-inline my-2 my-lg-0">
-      <button type="button" class="btn btn-info" style="margin-right: 100px;">REGISTER</button>
+      <button type="button" class="btn btn-info" style="margin-right: 100px; background: #5bc0be; background-color: #5bc0be; font-weight: bold; border-color: white; margin-right: 60px;">REGISTER</button>
     </form>
   </div>
 </nav>
-<!-- END BOOTSTRAP -->
-
-<!-- <section class="coba">
-	<div class="background-holder">
-		<img class="pict" src="image/home.jpg">
-	</div>
-</section> -->
-
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-        <img src="image/home.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h1 class="judul1">Belajar Coding Sekarang!</h1>
-          <p class="lead">Lebih dari 2.000 modul belajar pemrograman dalam beragam format yang bisa kamu pelajari.</p>
-
-         <div class="row">
-        	<div class="col-md-8">
-        		<input class ="cari" type="text" name="cari" placeholder="Cari tutorial, e-book atau materi belajar coding lainnya">
-        	</div>
-
-        	<div class="col-md-4">
-        		<button class="btn btn--primary type--uppercase mt-0" type="submit">
-        			CARI
-        		</button>
-        	</div>
-
-         </div>
-        </div>
-
-
-    </div>
-<!--     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div> -->
-  </div>
-</div>
+<!-- END BOOTSTRAP HEADER-->
 
 </body>
 </html>
